71eea638ff286b631ad9eee75f1b35e49c17112a
rohittt-29 <rm2193352@gmail.com>
2025-06-08 15:10:45 +0530

    Routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import rohitImg from '../assets/rohit.png';
+const Navbar = () => {
+  return (
+ <div className="navbar bg-base-300 shadow-sm">
+   <div className="flex-1">
+     <a className="btn btn-ghost text-xl">daisyUI</a>
+   </div>
+   <div className="flex gap-2">
+    
+     <div className="dropdown dropdown-end mx-10">
+       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+         <div className="w-10 rounded-full">
+           <img
+             alt="Tailwind CSS Navbar component"
+            src={rohitImg} className='w-10 h-10' />
+         </div>
+       </div>
+       <ul
+         tabIndex={0}
+         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+         <li>
+           <a className="justify-between">
+             Profile
+             <span className="badge">New</span>
+           </a>
+         </li>
+         <li><a>Settings</a></li>
+         <li><a>Logout</a></li>
+       </ul>
+     </div>
+   </div>
+ </div>
+  )
+}
+
+export default Navbar
